7ff57cd1d50a65a581bfee1403bf50e6f54b04fc
Alfian <viansmada100@gmail.com>
2020-10-28 14:21:53 +0700

    lol

---
+++ lib/pages/countyPage.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:tgd_covid_tracker/pages/search.dart';
+
+class CountryPage extends StatefulWidget {
+  @override
+  _CountryPageState createState() => _CountryPageState();
+}
+
+class _CountryPageState extends State<CountryPage> {
+  List countryData;
+
+  fetchCountryData() async {
+    http.Response response =
+        await http.get('https://api.kawalcorona.com/indonesia/provinsi');
+    setState(() {
+      countryData = json.decode(response.body);
+    });
+  }
+
+  @override
+  void initState() {
+    fetchCountryData();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.search),onPressed: (){
+
+            showSearch(context: context, delegate: Search(countryData));
+
+          },)
+        ],
+        title: Text('Provinsi Stats'),
+      ),
+      body: countryData == null
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Container(
+                    height: 130,
+                    margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          width: 200,
+                          margin: EdgeInsets.symmetric(horizontal: 10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                countryData[index]['attributes']['Provinsi'],
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                            child: Container(
+                          child: Column(
+                            
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                'CONFIRMED:' +
+                                    countryData[index]['attributes']['Kasus_Posi'].toString(),
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.red),
+                              ),
+                              Text(
+                                'RECOVERED:' +
+                                    countryData[index]['attributes']['Kasus_Semb'].toString(),
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.green),
+                              ),
+                              Text(
+                                'DEATHS:' +
+                                    countryData[index]['attributes']['Kasus_Meni'].toString(),
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Theme.of(context).brightness==Brightness.dark?Colors.grey[100]:Colors.grey[900]),
+                              ),
+                            ],
+                          ),
+                        ))
+                      ],
+                    ),
+                  ),
+                );
+              },
+              itemCount: countryData == null ? 0 : countryData.length,
+            ),
+    );
+  }
+}
